4dc158297da36b513512f009c8f6625831eb7323
Nitin <redknitin@gmail.com>
2014-07-07 21:25:58 +0400

    Signed-off-by: Nitin <redknitin@gmail.com>

---
+++ CmdlineTwitter/MainMenu.cs
@@ -0,0 +1,77 @@
+﻿using LinqToTwitter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdlineTwitter
+{
+    class MainMenu
+    {
+        public static void ShowMenu()
+        {
+            string menuOptions = "\r\n (1) Send Tweet\r\n (2) View Timeline\r\n (3) View Mentions\r\n (4) Exit\r\n";
+            Console.WriteLine(menuOptions);
+        }
+
+        static void SendTweet()
+        {
+            Console.Write("Enter Text: ");
+            string txt = Console.ReadLine().Trim();
+            ctx.UpdateStatus(txt);
+        }
+
+        static void ViewTimeline()
+        {
+            var qry = ctx.Status.Where(m => m.Type == StatusType.Home).OrderBy(m => m.CreatedAt);
+            foreach (var tw in qry)
+            {
+                string showRow = "[" + tw.User.Name + "]: " + tw.Text + "\r\n"; //tw.StatusID + 
+                Console.WriteLine(showRow);
+            }
+        }
+
+        static void ViewMentions()
+        {
+            var qry = ctx.Status.Where(m => m.Type == StatusType.Mentions).OrderBy(m => m.CreatedAt);
+            foreach (var tw in qry)
+            {
+                string showRow = "[" + tw.User.Name + "]: " + tw.Text + "\r\n"; //tw.StatusID + 
+                Console.WriteLine(showRow);
+            }
+        }
+
+        public static TwitterContext ctx;
+
+        public static void DoSomething() {
+            string inp = Console.ReadLine().Trim();
+            int optvals;
+            if (int.TryParse(inp, out optvals))
+            {
+                switch (optvals)
+                {
+                    case 1:
+                        SendTweet();
+                        break;
+                    case 2:
+                        ViewTimeline();
+                        break;
+                    case 3:
+                        ViewMentions();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
+        }
+    }
+}
---
+++ CmdlineTwitter/Program.cs
@@ -0,0 +1,54 @@
+﻿using LinqToTwitter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdlineTwitter
+{
+    class Program
+    {
+        public static PinAuthorizer auth;
+
+        static void authorize()
+        {
+            auth = new PinAuthorizer()
+            {
+                Credentials = new InMemoryCredentials
+                {
+                    ConsumerKey = "FiLff3SigcHPhHQxsSmpVQ", //ConfigurationManager.AppSettings["consumerKey"],
+                    ConsumerSecret = "xzmPCd8drcHurgaDG3SRdRooZCBzf1ZvgmekadUYZcQ" //ConfigurationManager.AppSettings["consumerSecret"]                    
+                },
+                GoToTwitterAuthorization = pageLink => Process.Start(pageLink),
+                GetPin = () =>
+                {
+                    Console.Write("Enter Twitter PIN: ");
+                    var input = Console.ReadLine().Trim();
+                    return input;
+                },
+                AuthAccessType = AuthAccessType.Write
+            };
+
+            auth.Authorize();
+
+            if (!auth.IsAuthorized)
+            {
+                Console.WriteLine("Not authorized");
+                Environment.Exit(0);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            authorize();
+            MainMenu.ctx = new TwitterContext(auth);
+
+            while(true) {
+                MainMenu.ShowMenu();
+                MainMenu.DoSomething();
+            }
+        }
+    }
+}
